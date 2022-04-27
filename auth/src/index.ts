908670b02cb81e62558391cbd7b29b8238524ada
michalovsky <michalovskyyy@gmail.com>
2022-04-27 15:36:36 +0200

    add initial app for auth service

---
+++ auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!");
+});
