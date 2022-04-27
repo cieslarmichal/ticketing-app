95c3b1cab2a8735c80ce2d52b850dd2f08979eb5
michalovsky <michalovskyyy@gmail.com>
2022-04-27 16:17:50 +0200

    add ingress service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hello");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
