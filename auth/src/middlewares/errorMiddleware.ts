a6cce749b20775e7f9e5b9375b914183921150ce
michalovsky <michalovskyyy@gmail.com>
2022-04-27 20:00:23 +0200

    add handling different kind of errors in error middleware

--- auth/src/middlewares/errorMiddleware.ts
+++ auth/src/middlewares/errorMiddleware.ts
@@ -1,8 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { DatabaseConnectionError, RequestValidationError } from '../errors';
 
 export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log('Something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.reasons.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
+    res.status(StatusCodes.BAD_REQUEST).send({ errors: formattedErrors });
+    return;
+  } else if (err instanceof DatabaseConnectionError) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ errors: [{ message: err.message }] });
+    return;
+  }
 
-  res.status(StatusCodes.BAD_REQUEST).send({ message: err.message });
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ errors: [{ message: err.message }] });
 };
