40616b6f3c0ed966ae67b656dc5df82707de1ccc
michalovsky <michalovskyyy@gmail.com>
2022-04-29 12:57:11 +0200

    remove password from user response

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -15,16 +15,28 @@ interface UserModel extends mongoose.Model<UserDocument> {
   build(attributes: UserAttributes): UserDocument;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
   },
-});
+);
 
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
