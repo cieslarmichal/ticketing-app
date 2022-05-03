b9566cf73867aaf565c175784b076b2ef370d7f4
michalovsky <michalovskyyy@gmail.com>
2022-05-03 20:26:15 +0200

    add error handling in signup page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
-export default ({Component, pageProps}) => {
-    return <Component {...pageProps}/>;
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
 };
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,27 +1,52 @@
-import { useState } from "react";
+import { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
-    const onSubmit = (event) => {
-        event.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault();
 
-        console.log(email, password);
-    };
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
 
-    return (
+      console.log(response.data);
+    } catch (error) {
+      setErrors(error.response.data.errors);
+    }
+  };
+
+  return (
     <form onSubmit={onSubmit}>
-        <h1>Sign Up</h1>
-        <div className="form-group">
-            <label>Email Address</label>
-            <input value={email} onChange={event => setEmail(event.target.value)} className="form-control"/>
+      <h1>Sign Up</h1>
+      <div className="form-group">
+        <label>Email Address</label>
+        <input value={email} onChange={(event) => setEmail(event.target.value)} className="form-control" />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <ul className="my-0">
+            {errors.map((error) => (
+              <li key={error.message}>{error.message}</li>
+            ))}
+          </ul>
         </div>
-        <div className="form-group">
-            <label>Password</label>
-            <input value={password} onChange={event => setPassword(event.target.value)} type="password" className="form-control"/>
-        </div>
-        <button className="btn btn-primary">Sign Up</button>
+      )}
+      <button className="btn btn-primary">Sign Up</button>
     </form>
-    );
+  );
 };
