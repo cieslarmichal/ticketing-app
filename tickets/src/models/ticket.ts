48c44d60871a621f333405828a2c2fda42990177
michalovsky <michalovskyyy@gmail.com>
2022-05-04 22:47:48 +0200

    add ticket model

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+
+interface TicketAttributes {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketDocument extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDocument> {
+  build(attributes: TicketAttributes): TicketDocument;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+      versionKey: false,
+    },
+  },
+);
+
+ticketSchema.statics.build = (attributes: TicketAttributes) => {
+  return new Ticket(attributes);
+};
+
+const Ticket = mongoose.model<TicketDocument, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
