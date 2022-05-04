384e32b4f08a513ab4322789da65e99ef42f9bae
michalovsky <michalovskyyy@gmail.com>
2022-05-04 16:12:13 +0200

    extract building axios client to separate file

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
-  return <h1>Hello</h1>;
+import buildClient from '../api/buildClient';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
 };
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
