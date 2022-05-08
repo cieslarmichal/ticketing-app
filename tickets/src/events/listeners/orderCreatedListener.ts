426782007b8e4877ea738b0b868062d2774bee58
michalovsky <michalovskyyy@gmail.com>
2022-05-08 13:01:08 +0200

    add order created listener

---
+++ tickets/src/events/listeners/orderCreatedListener.ts
@@ -0,0 +1,29 @@
+import { EventBusListener, Subject, OrderCreatedEvent } from '@cieslar-ticketing-common/common';
+import { Message } from 'node-nats-streaming';
+import { TicketNotFoundError } from 'src/errors';
+import { Ticket } from '../../models';
+import { queueGroupName } from './queueGroupName';
+
+export class OrderCreatedListener extends EventBusListener<OrderCreatedEvent> {
+  readonly subject: Subject.OrderCreated = Subject.OrderCreated;
+  readonly queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], message: Message) {
+    const {
+      id: orderId,
+      ticket: { id: ticketId },
+    } = data;
+
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new TicketNotFoundError();
+    }
+
+    ticket.orderId = orderId;
+
+    await ticket.save();
+
+    message.ack();
+  }
+}
