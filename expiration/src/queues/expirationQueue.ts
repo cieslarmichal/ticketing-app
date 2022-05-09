3f7da22d9179ac11326e03e4ff624191a958d03d
michalovsky <michalovskyyy@gmail.com>
2022-05-09 17:32:21 +0200

    add expiration queue

---
+++ expiration/src/queues/expirationQueue.ts
@@ -0,0 +1,17 @@
+import Queue from 'bull';
+
+interface Payload {
+  orderId: string;
+}
+
+const expirationQueue = new Queue<Payload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log(job.data);
+});
+
+export { expirationQueue };
