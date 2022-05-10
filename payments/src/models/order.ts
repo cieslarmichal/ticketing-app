6df79b2d54b0198398dcfdbad46e6da18188cc46
michalovsky <michalovskyyy@gmail.com>
2022-05-10 12:36:59 +0200

    add order model in payments microservice

---
+++ payments/src/models/order.ts
@@ -0,0 +1,67 @@
+import { OrderStatus } from '@cieslar-ticketing-common/common';
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+interface OrderAttributes {
+  id: string;
+  version: string;
+  status: OrderStatus;
+  userId: string;
+  price: number;
+}
+
+export interface OrderDocument extends mongoose.Document {
+  version: number;
+  status: OrderStatus;
+  userId: string;
+  price: number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDocument> {
+  build(attributes: OrderAttributes): OrderDocument;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+      versionKey: false,
+    },
+  },
+);
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attributes: OrderAttributes) => {
+  return new Order({
+    _id: attributes.id,
+    version: attributes.version,
+    userId: attributes.userId,
+    status: attributes.status,
+    price: attributes.price,
+  });
+};
+
+const Order = mongoose.model<OrderDocument, OrderModel>('Order', orderSchema);
+
+export { Order };
