afd226f23d2d6c55d0e4fda0f6510eae1138dab3
michalovsky <michalovskyyy@gmail.com>
2022-05-11 11:06:54 +0200

    add get ticket page

---
+++ client/pages/tickets/[ticketId].js
@@ -0,0 +1,34 @@
+import Router from 'next/router';
+import { useRequest } from '../../hooks';
+
+const GetTicket = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: { ticketId: ticket.id },
+    onSuccess: () => {
+      Router.push('/');
+    },
+  });
+
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={doRequest} className="btn btn-primary">
+        Purchase
+      </button>
+    </div>
+  );
+};
+
+GetTicket.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  return { ticket: data };
+};
+
+export default GetTicket;
